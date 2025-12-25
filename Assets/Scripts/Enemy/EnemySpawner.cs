7c5c4d833b1e3f5c974e13de97777e7c1b355c5e
AlanRC9 <alanrb9@gmail.com>
2025-12-25 20:17:16 +0100

    Spawn

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -2,17 +2,44 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float enemySpeed;
     [SerializeField] private float spawnRate;
     [SerializeField] private float spawnRange;
+    private float fireTimer;
+    private float timeSinceLastSpawnRateChange;
+    private float initialSpawnRate;
 
     private void Start()
     {
+        fireTimer = spawnRate;
         InvokeRepeating("SpawnEnemy", 0, spawnRate);
+        // initialSpawnRate = spawnRate;
+        // timeSinceLastSpawnRateChange = 0f;
+    }
+
+    private void Update() {
+        fireTimer -= Time.deltaTime;
+        // timeSinceLastSpawnRateChange += Time.deltaTime;
+        
+        // // Every 30 seconds, reduce the spawnRate by 10% (make it 10% faster)
+        // if (timeSinceLastSpawnRateChange >= 30f)
+        // {
+        //     spawnRate = Mathf.Max(0.1f, spawnRate * 0.9f); // Reduce the spawnRate by 10% (multiply by 0.9)
+        //     timeSinceLastSpawnRateChange = 0f;
+        // }
+        
+        // Enemies spawner based on timer.
+        if (fireTimer <= 0f)
+        {
+            spawnRate = Mathf.Max(0.1f, spawnRate * 0.9f); // Increase enemies spawnRate in 10% every 30 seconds.
+            fireTimer = spawnRate;
+        }
     }
     
     private void SpawnEnemy()
     {
-        Instantiate(enemyPrefab, RandomStartPosition(), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemyPrefab, RandomStartPosition(), Quaternion.identity);
+        newEnemy.GetComponent<Rigidbody>().linearVelocity = new Vector3(0, 0, -enemySpeed);
     }
     
     private Vector3 RandomStartPosition()
